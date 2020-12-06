837a21d0b23790e82c713a0c54fa5cab2906fb78
BN001721962 <rifqi.arislan@binus.ac.id>
2020-12-06 20:50:57 +0700

    adding new file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,18 +9,23 @@
     @endif
 <body>
 <div class="row">
-    @foreach($product as $prod)
-        <div class="col-sm-4">
-            <div class="card" style="width: 300px">
-                <img src="{{asset($prod->image)}}" alt="" class="image" style="width: 100%">
-                <div class="card-body">
-                    <h4 class="card-title">{{$prod->name}}</h4>
-                    <p class="card-text">{{$prod->description}}</p>
-{{--                    <a href="{{route('', $prod->id)}}}" class="btn btn-primary">See Product</a>--}}
+    @if($product->isNotEmpty())
+        @foreach($product as $prod)
+            <div class="col-sm-4">  67
+                <div class="card" style="width: 300px">
+                    <img src="{{asset($prod->image)}}" alt="" class="image" style="width: 100%">
+                    <div class="card-body">
+                        <h4 class="card-title">{{$prod->name}}</h4>
+                        <p class="card-text">{{$prod->description}}</p>
+                        {{--                    <a href="{{route('', $prod->id)}}}" class="btn btn-primary">See Product</a>--}}
+                    </div>
                 </div>
             </div>
+        @endforeach
+    @else
+        <div class="notfound">
+            There is no product match with the keyword.
         </div>
-    @endforeach
     <div class="paginate">
         {{$product->links()}}
     </div>
